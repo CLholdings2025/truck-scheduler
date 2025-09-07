d98daf2d2b5ca35f086bdb54762ae46acfcb0a77
CLholdings2025 <juan@clholdings.co.za>
2025-09-07 16:25:30 -0700

    Update env.js

--- public/env.js
+++ public/env.js
@@ -1,4 +1,4 @@
 // public/env.js
 // Replace with your actual values from Supabase Project Settings → API.
-window.ENV_SUPABASE_URL = "https://YOUR-PROJECT-REF.supabase.co";
+window.ENV_SUPABASE_URL = "https://gdbgrzynozdndpzwtrbp.supabase.co";
 window.ENV_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdkYmdyenlub3pkbmRwend0cmJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3MDYxNjIsImV4cCI6MjA3MjI4MjE2Mn0.5xdmMJ5EpuuRxtHUvuqSw4eZcGot0gqN6Qo5E-fxqT8";
